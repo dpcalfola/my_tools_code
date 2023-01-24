a4963753609b769456710ae8da4b95af71aea9e9
Fola Flor <dpcalfola@gmail.com>
2023-01-24 22:04:15 +0900

    Feat > Complete the code (version 0.1)
        ** Complete the code
        not only the feature of branch
    
        < Goal >
        Get the total power consumption
        of a day, a month, a year
        from the device data
    
        < How to use >
        1. Make data variables
            - data_{device_id} = [name , power, cnt, use_time_a_day]
        2. Make data_list from each data variable
            - data_list = [data_{device_id}, data_{device_id}, ...]
        3. Run the code
    
        < Result >
        1. Total power consumption of a day
        2. Total power consumption of a month
        3. Total power consumption of a year
    
        ** To Use This Code, You Should Change The Data Variables **
    
        < My Data in the code >
        This code includes my data variables
        So you can see my result if you run the code
    
        < How to use your data >
        If you want to use your data,
        you should change the data variables and the data_list
        marked with """----- data part -----"""

---
+++ power_consumption/git_message.md
@@ -0,0 +1,44 @@
+# Git Message
+
+
+### 1.
+
+```text
+Feat > Complete the code (version 0.1) 
+
+    ** Complete the code
+    not only the feature of branch
+    
+    < Goal >
+    Get the total power consumption 
+    of a day, a month, a year
+    from the device data
+    
+    
+    < How to use >
+    1. Make data variables
+        - data_{device_id} = [name , power, cnt, use_time_a_day]
+    2. Make data_list from each data variable
+        - data_list = [data_{device_id}, data_{device_id}, ...]
+    3. Run the code
+    
+    
+    < Result >
+    1. Total power consumption of a day
+    2. Total power consumption of a month
+    3. Total power consumption of a year
+    
+    
+    
+    ** To Use This Code, You Should Change The Data Variables **   
+    
+    < My Data in the code > 
+    This code includes my data variables
+    So you can see my result if you run the code
+    
+    < How to use your data >
+    If you want to use your data,
+    you should change the data variables and the data_list
+    marked with """----- data part -----"""
+    
+```
--- power_consumption/main.py
+++ power_consumption/main.py
@@ -1,16 +1,78 @@
-# This is a sample Python script.
+"""
+This program is to calculate total power consumption
+of my family's hobby devices
+which are doubtfully high power consumption.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+Because I don't understand why my electricity bill is so high.
+"""
+
+"""----- class part -----"""
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class Device:
+
+    def __init__(self, name, power, cnt, use_time_a_day):
+        """
+        :param name: 기기 이름
+        :param power: 전력량(w)
+        :param cnt: 갯수(개)
+        :param use_time_a_day: 하루 사용 시간(h)
+        """
+
+        self.name: str = name
+        self.power: float = power
+        self.cnt: int = cnt
+        self.use_time_a_day: int = use_time_a_day
 
 
-# Press the green button in the gutter to run the script.
+class Electrics(Device):
+
+    def __init__(self, name, power, cnt, use_time_a_day):
+        super().__init__(name, power, cnt, use_time_a_day)
+
+    # calculate daily power (unit: kWh)
+    @property
+    def daily_power(self) -> float:
+        return self.power * self.cnt * self.use_time_a_day / 1000
+
+    # Calculate total power (unit: KWh)
+    def total_power(self, time_case: str) -> float:
+        if time_case == 'a_day':
+            return self.daily_power
+        elif time_case == 'a_month':
+            return self.daily_power * 30
+        elif time_case == 'a_year':
+            return self.daily_power * 365
+        else:
+            raise ValueError('time_case should be a_day, a_month, a_year')
+
+
+"""----- data part -----"""
+# data: list = [name, power, cnt, use_time_a_day]
+data_lamp_1 = ['PLED', 20, 4, 12]
+data_lamp_2 = ['Philips', 16, 4, 12]
+data_lamp_3 = ['FarmTec', 12, 1, 12]
+data_lamp_4 = ['LED_BAR', 7.2, 10, 12]
+data_nas = ['Synology', 25, 1, 24]
+
+# data_list
+data_list: list = [data_lamp_1, data_lamp_2, data_lamp_3, data_lamp_4, data_nas]
+
+"""----- main part -----"""
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Make a list of devices objects from data_list
+    devices: list[Electrics] = [Electrics(*data) for data in data_list]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Calculate total power (unit: kWh)
+    total_power_a_day: float = 0
+    total_power_a_month: float = 0
+    total_power_a_year: float = 0
+
+    for device in devices:
+        total_power_a_day += device.total_power('a_day')
+        total_power_a_month += device.total_power('a_month')
+        total_power_a_year += device.total_power('a_year')
+
+    print(f'Total power a day: {total_power_a_day:.3f} kWh')
+    print(f'Total power a month: {total_power_a_month:.3f} kWh')
+    print(f'Total power a year: {total_power_a_year:.3f} kWh')
